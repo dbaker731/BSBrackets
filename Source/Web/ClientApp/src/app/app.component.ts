3dc75eb5790bb329963ca65e8799ebf767b2e1dd
Daniel Baker <dbaker731@gmail.com>
2021-08-16 18:43:00 -0500

    Created user service, set up SSL

--- Source/Web/ClientApp/src/app/app.component.ts
+++ Source/Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './_services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BS Brackets';
+  users: any;
+
+  constructor(private usersSerive: UsersService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.usersSerive.getUsers().subscribe( result => {
+      this.users = result;
+    }, err => {
+      console.log(err);
+    })
+  }
 }
--- Source/Web/ClientApp/src/app/app.module.ts
+++ Source/Web/ClientApp/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
