8ccdb43cc9fcf524121716990a03287ad1355152
Daniel Baker <dbaker731@gmail.com>
2021-08-16 15:21:54 -0500

    Fixing file structure, adding app routing module to angular

---
+++ Source/BSBRackets/Logic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+using Repository;
+
+namespace BSBRackets
+{
+    public class Logic
+    {
+        private readonly Repo _repo;
+        private readonly Mapper _mapper;
+        public Logic(Repo repo, Mapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        // User controller
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            return await _repo.GetUsers();
+        }
+
+        public async Task<AppUser> GetUserById(int id)
+        {
+            return await _repo.GetUserById(id);
+        }
+
+        public AppUser GetUserByIdNonAsync(int id)
+        {
+            return _repo.GetUserByIdNonAsync(id);
+        }
+    }
+}
---
+++ Source/Repository/Repo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Repository
+{
+    public class Repo
+    {
+        private readonly BSDbContext _context;
+        DbSet<AppUser> users;
+
+        public Repo(BSDbContext context)
+        {
+            _context = context;
+            users = _context.Users;
+        }
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            return await users.ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserById(int id)
+        {
+            return await users.FirstOrDefaultAsync(users => users.Id == id);
+        }
+        // do we use this?
+        public AppUser GetUserByIdNonAsync(int id)
+        {
+            return users.FirstOrDefault(users => users.Id == id);
+        }
+    }
+}
