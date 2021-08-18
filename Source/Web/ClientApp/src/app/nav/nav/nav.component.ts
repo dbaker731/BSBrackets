67fdf93cca3b2cecffd8ae162996235998a0a0d2
Daniel Baker <dbaker731@gmail.com>
2021-08-17 22:19:46 -0500

    beginning of navbar

---
+++ Source/Web/ClientApp/src/app/nav/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor(public accountService: AccountService, private router: Router) { }
+
+  model: any = {}
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(data => {
+      this.router.navigateByUrl('/members');
+    }, err => {
+      console.log(err);
+      // this.toastr.error(err.error);
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
+
+}
